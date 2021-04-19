0248cddd8aa81d249eec924ac532a64de82c0a3a
thajones <77893270+thajones@users.noreply.github.com>
2021-04-19 22:26:08 +0530

    model in list

--- assets/js/customerindex.js
+++ assets/js/customerindex.js
@@ -1,22 +1,20 @@
-includeHTML();
-$("#example1")
-  .DataTable({
-    responsive: true,
-    lengthChange: false,
-    autoWidth: false,
-    paging: true,
-    ordering: false,
-    searching: true,
-  })
-  .buttons()
-  .container()
-  .appendTo("#example1_wrapper .col-md-6:eq(0)");
-$("#example1_wrapper").children("div:first-child").attr("id", "yoyo");
-$("#yoyo")
-  .children("div:first-child")
-  .append(
-    '<a href="new_customer.html" class="btn btn-primary vip">Add New Customer</a>'
-  );
+$(function () {
+  $("#example1")
+    .DataTable({
+      responsive: true,
+      lengthChange: false,
+      autoWidth: false,
+      paging: true,
+      ordering: false,
+      searching: true,
+    });
+  $("#example1_wrapper").children("div:first-child").attr("id", "yoyo");
+  $("#yoyo")
+    .children("div:first-child")
+    .append(
+      '<a href="new_customer.html" class="btn btn-primary vip">Add New Customer</a>'
+    );
+});
 $(".sublist").click(function () {
   var parent_id = $(this).parent("tr").attr("data-href");
   window.location = parent_id;
@@ -28,7 +26,7 @@ $(".statement").click(function () {
 $(".trash").click(function () {
   var a = $(this).attr("id");
   $("#trashid").val(a);
-  $("modal-default").click();
+  $("#modelactivate").click();
   // $("#list").hide();
   // $("#trash").show();
 });
--- views/customers/index.php
+++ views/customers/index.php
@@ -35,13 +35,66 @@
                           <i class="far fa-file-alt statement"></i>
                         </td>
                         <td>
+                          <i class="fas fa-trash-alt trash" id="1"></i>
+                        </td>
+                      </tr>
+                      <tr data-href="new_customer.html">
+                        <td class="sublist">Customer 1</td>
+                        <td class="sublist">₹<span>15.00</span></td>
+                        <td class="sublist">20/02/2020</td>
+                        <td class="sublist">10000</td>
+                        <td class="sublist">prashant</td>
+                        <td class="sublist">Default</td>
+                        <td data-href="new_customer.html">
+                          <i class="far fa-file-alt statement"></i>
+                        </td>
+                        <td>
+                          <i class="fas fa-trash-alt trash" id="2"></i>
+                        </td>
+                      </tr><tr data-href="new_customer.html">
+                        <td class="sublist">Customer 1</td>
+                        <td class="sublist">₹<span>15.00</span></td>
+                        <td class="sublist">20/02/2020</td>
+                        <td class="sublist">10000</td>
+                        <td class="sublist">prashant</td>
+                        <td class="sublist">Default</td>
+                        <td data-href="new_customer.html">
+                          <i class="far fa-file-alt statement"></i>
+                        </td>
+                        <td>
+                          <i class="fas fa-trash-alt trash" id="3"></i>
+                        </td>
+                      </tr><tr data-href="new_customer.html">
+                        <td class="sublist">Customer 1</td>
+                        <td class="sublist">₹<span>15.00</span></td>
+                        <td class="sublist">20/02/2020</td>
+                        <td class="sublist">10000</td>
+                        <td class="sublist">prashant</td>
+                        <td class="sublist">Default</td>
+                        <td data-href="new_customer.html">
+                          <i class="far fa-file-alt statement"></i>
+                        </td>
+                        <td>
+                          <i class="fas fa-trash-alt trash" id="4"></i>
+                        </td>
+                      </tr><tr data-href="new_customer.html">
+                        <td class="sublist">Customer 1</td>
+                        <td class="sublist">₹<span>15.00</span></td>
+                        <td class="sublist">20/02/2020</td>
+                        <td class="sublist">10000</td>
+                        <td class="sublist">prashant</td>
+                        <td class="sublist">Default</td>
+                        <td data-href="new_customer.html">
+                          <i class="far fa-file-alt statement"></i>
+                        </td>
+                        <td>
                           <i class="fas fa-trash-alt trash" id="5"></i>
                         </td>
                       </tr>
                     </tbody>
                   </table>
                 </div>
-                <div class="card-body p-3" id="trash" style="display: none">
+                <!-- <div class="card-body p-3" id="trash" style="display: none">
                   <form action="" method="post" class="text-center">
                     <p>Are you sure you want to delete?</p>
                     <input type="hidden" id="trashid" name="deactivate" />
@@ -50,8 +103,33 @@
                     </button>
                     <a class="list btn btn-light">Cancel</a>
                   </form>
-                </div>
+                </div> -->
               </div>
+            </div>
+          </div>
+        </div>
+        <button type="button" id="modelactivate" style="display: none;" data-toggle="modal" data-target="#modal-default"></button>
+        <div class="modal fade" id="modal-default">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <form action="" method="post" class="text-center mb-0">
+                <div class="modal-header">
+                  <h4 class="modal-title">Confirm Delete</h4>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <p>Are you sure you want to delete ?<span id="username"></span></p>
+                  <input type="hidden" id="trashid" name="deactivate" value="" />
+                </div>
+                <div class="modal-footer justify-content-between">
+                  <button type="submit" class="btn btn-danger btn-sm">
+                    Delete
+                  </button>
+                  <a class="list btn btn-light btn-sm" data-dismiss="modal">Cancel</a>
+                </div>
+              </form>
             </div>
           </div>
         </div>
