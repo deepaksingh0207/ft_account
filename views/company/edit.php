e5cdf899aba1615b2e137d15b0d71167af60f3d6
thajones <jones.thayil@outlook.com>
2021-05-17 20:19:00 +0530

    fax pattern removed

--- views/company/edit.php
+++ views/company/edit.php
@@ -64,7 +64,7 @@
 												<label for="id_fax"> Fax </label>
 											</div>
 											<div class="col-sm-12 col-lg-3 form-group numberonly">
-												<input type="tel" class="form-control fc ftsm" name="fax" id="id_fax" pattern="[0-9]" minlength="10" maxlength="15" value="<?php echo $customer['fax'] ?>" />
+												<input type="tel" class="form-control fc ftsm" name="fax" id="id_fax" minlength="10" maxlength="15" value="<?php echo $customer['fax'] ?>" />
 											</div>
 										</div>
 										<div class="row mx-1">
