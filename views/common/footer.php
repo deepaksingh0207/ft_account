83d66ec15e4d79820a403ae919fac0141e417068
thajones <jones.thayil@gmail.com>
2021-06-04 17:07:21 +0530

    resolved sidebar JS issue

--- views/common/footer.php
+++ views/common/footer.php
@@ -1,10 +1,9 @@
+<script src="<?php echo ROOT; ?>assets/plugins/select2/js/select2.full.min.js"></script>
+<script src="<?php echo ROOT ?>assets/plugins/toastr/toastr.min.js"></script>
 
 <?php if ($controller == 'users') : ?>
-  
   <script src="<?php echo ROOT; ?>assets/plugins/jquery-validation/jquery.validate.min.js"></script>
   <script src="<?php echo ROOT; ?>assets/plugins/jquery-validation/additional-methods.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/plugins/select2/js/select2.full.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/dist/js/adminlte.min.js"></script>
   <script src="<?php echo ROOT; ?>assets/js/login.js?<?php echo time(); ?>"></script>
   <script src="<?php echo ROOT; ?>assets/js/custom.js"></script>
 <?php endif; ?>
@@ -15,11 +14,27 @@
   <script src="<?php echo ROOT; ?>assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
   <script src="<?php echo ROOT; ?>assets/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
   <script src="<?php echo ROOT; ?>assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/plugins/select2/js/select2.full.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/dist/js/adminlte.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/dist/js/demo.js"></script>
   <script src="<?php echo ROOT; ?>assets/js/customerindex.js"></script>
   <script src="<?php echo ROOT; ?>assets/js/custom.js"></script>
+<?php endif; ?>
+
+<?php if ($controller == 'customers' && ($action == 'view')) : ?>
+  <script src="<?php echo ROOT; ?>assets/js/custom.js"></script>
+<?php endif; ?>
+
+<?php if ($controller == 'customers' && ($action == 'edit')) : ?>
+  <script src="<?php echo ROOT; ?>assets/plugins/jquery-validation/jquery.validate.min.js"></script>
+  <script src="<?php echo ROOT; ?>assets/plugins/jquery-validation/additional-methods.min.js"></script>
+  <script src="<?php echo ROOT; ?>assets/js/custom.js"></script>
+  <script src="<?php echo ROOT; ?>assets/js/edit_customer.js?<?php echo time(); ?>"></script>
+<?php endif; ?>
+
+
+<?php if ($controller == 'customers' && ($action == 'create')) : ?>
+  <script src="<?php echo ROOT; ?>assets/plugins/jquery-validation/jquery.validate.min.js"></script>
+  <script src="<?php echo ROOT; ?>assets/plugins/jquery-validation/additional-methods.min.js"></script>
+  <script src="<?php echo ROOT; ?>assets/js/custom.js"></script>
+  <script src="<?php echo ROOT; ?>assets/js/new_customer.js"></script>
 <?php endif; ?>
 
 <?php if ($controller == 'company' && ($action == 'index')) : ?>
@@ -27,44 +42,31 @@
   <script src="<?php echo ROOT; ?>assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
   <script src="<?php echo ROOT; ?>assets/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
   <script src="<?php echo ROOT; ?>assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/plugins/select2/js/select2.full.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/dist/js/adminlte.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/dist/js/demo.js"></script>
   <script src="<?php echo ROOT; ?>assets/js/companyindex.js"></script>
   <script src="<?php echo ROOT; ?>assets/js/custom.js"></script>
 <?php endif; ?>
 
-
-<?php if ($controller == 'customers' && ($action == 'create') || ($action == 'edit') || ($action == 'view')) : ?>
-  <script src="<?php echo ROOT; ?>assets/plugins/jquery-validation/jquery.validate.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/plugins/jquery-validation/additional-methods.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/plugins/select2/js/select2.full.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/dist/js/adminlte.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/dist/js/demo.js"></script>
-  <script src="<?php echo ROOT; ?>assets/plugins/bs-stepper/js/bs-stepper.min.js"></script>
+<?php if ($controller == 'company' && ($action == 'create') || ($action == 'edit') || ($action == 'view')) : ?>
   <script src="<?php echo ROOT; ?>assets/js/custom.js"></script>
 <?php endif; ?>
 
-<?php if ($controller == 'customers' && ($action == 'edit')) : ?>
-  <script src="<?php echo ROOT; ?>assets/js/edit_customer.js?<?php echo time(); ?>"></script>
+<?php if ($controller == 'company' && ($action == 'create')) : ?>
+  <script src="<?php echo ROOT; ?>assets/plugins/jquery-validation/jquery.validate.min.js"></script>
+  <script src="<?php echo ROOT; ?>assets/plugins/jquery-validation/additional-methods.min.js"></script>
+  <script src="<?php echo ROOT; ?>assets/js/custom.js"></script>
+  <script src="<?php echo ROOT; ?>assets/js/new_company.js?<?php echo time(); ?>"></script>
 <?php endif; ?>
 
-
-<?php if ($controller == 'customers' && ($action == 'create')) : ?>
-  <script src="<?php echo ROOT; ?>assets/js/new_customer.js"></script>
-<?php endif; ?>
-
-<?php if ($controller == 'orders' && ($action == 'view')) : ?>
-  <script src="<?php echo ROOT; ?>assets/dist/js/adminlte.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/dist/js/demo.js"></script>
+<?php if ($controller == 'company' && ($action == 'edit')) : ?>
+  <script src="<?php echo ROOT; ?>assets/plugins/jquery-validation/jquery.validate.min.js"></script>
+  <script src="<?php echo ROOT; ?>assets/plugins/jquery-validation/additional-methods.min.js"></script>
+  <script src="<?php echo ROOT; ?>assets/js/custom.js"></script>
+  <script src="<?php echo ROOT; ?>assets/js/edit_company.js?<?php echo time(); ?>"></script>
 <?php endif; ?>
 
 <?php if ($controller == 'orders' && ($action == 'create')) : ?>
   <script src="<?php echo ROOT; ?>assets/plugins/jquery-validation/jquery.validate.min.js"></script>
   <script src="<?php echo ROOT; ?>assets/plugins/jquery-validation/additional-methods.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/plugins/select2/js/select2.full.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/dist/js/adminlte.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/dist/js/demo.js"></script>
   <script src="<?php echo ROOT; ?>assets/js/new_order.js?<?php echo time(); ?>"></script>
   <script src="<?php echo ROOT; ?>assets/js/custom.js"></script>
 <?php endif; ?>
@@ -75,9 +77,6 @@
   <script src="<?php echo ROOT; ?>assets/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
   <script src="<?php echo ROOT; ?>assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
   <script src="<?php echo ROOT ?>assets/plugins/toastr/toastr.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/plugins/select2/js/select2.full.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/dist/js/adminlte.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/dist/js/demo.js"></script>
   <script src="<?php echo ROOT; ?>assets/js/orderindex.js"></script>
   <script src="<?php echo ROOT; ?>assets/js/custom.js"></script>
 <?php endif; ?>
@@ -85,9 +84,6 @@
 <?php if ($controller == 'invoices' && ($action == 'create')) : ?>
   <script src="<?php echo ROOT; ?>assets/plugins/jquery-validation/jquery.validate.min.js"></script>
   <script src="<?php echo ROOT; ?>assets/plugins/jquery-validation/additional-methods.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/plugins/select2/js/select2.full.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/dist/js/adminlte.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/dist/js/demo.js"></script>
   <script src="<?php echo ROOT; ?>assets/js/new_invoice.js?<?php echo time(); ?>"></script>
   <script src="<?php echo ROOT; ?>assets/js/custom.js"></script>
 <?php endif; ?>
@@ -97,32 +93,12 @@
   <script src="<?php echo ROOT; ?>assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
   <script src="<?php echo ROOT; ?>assets/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
   <script src="<?php echo ROOT; ?>assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-  <script src="<?php echo ROOT ?>assets/plugins/toastr/toastr.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/plugins/select2/js/select2.full.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/dist/js/adminlte.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/dist/js/demo.js"></script>
   <script src="<?php echo ROOT; ?>assets/js/invoiceindex.js"></script>
   <script src="<?php echo ROOT; ?>assets/js/custom.js"></script>
 <?php endif; ?>
 
-<?php if ($controller == 'company' && ($action == 'create') || ($action == 'edit') || ($action == 'view')) : ?>
-  <script src="<?php echo ROOT; ?>assets/plugins/jquery-validation/jquery.validate.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/plugins/jquery-validation/additional-methods.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/plugins/select2/js/select2.full.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/dist/js/adminlte.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/dist/js/demo.js"></script>
-  <!-- <script src="<?php echo ROOT; ?>assets/plugins/bs-stepper/js/bs-stepper.min.js"></script> -->
-  <script src="<?php echo ROOT; ?>assets/js/custom.js"></script>
-<?php endif; ?>
-
-<?php if ($controller == 'company' && ($action == 'create')) : ?>
-  <script src="<?php echo ROOT; ?>assets/js/new_company.js?<?php echo time(); ?>"></script>
-<?php endif; ?>
-
-<?php if ($controller == 'company' && ($action == 'edit')) : ?>
-  <script src="<?php echo ROOT; ?>assets/js/edit_company.js?<?php echo time(); ?>"></script>
-<?php endif; ?>
-
+<script src="<?php echo ROOT; ?>assets/dist/js/adminlte.min.js"></script>
+<script src="<?php echo ROOT; ?>assets/dist/js/demo.js"></script>
 </body>
 
 </html>
--- views/common/header.php
+++ views/common/header.php
@@ -11,26 +11,11 @@
   <title>F. T. Help & Support</title>
   <link rel="stylesheet" href="<?php echo ROOT; ?>assets/css/google_font.css" />
   <link rel="stylesheet" href="<?php echo ROOT ?>assets/plugins/toastr/toastr.min.css" />
-  <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/fontawesome-free/css/all.min.css" />
-  <script src="<?php echo ROOT; ?>assets/plugins/jquery/jquery.min.js"></script>
-  <script src="<?php echo ROOT; ?>assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-
-  <?php if ($controller == 'users' && ($action == 'login')) : ?>
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css" />
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/select2/css/select2.min.css">
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/dist/css/adminlte.min.css" />
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/css/parallax.css" />
-  <?php endif; ?>
-
+  <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/select2/css/select2.min.css">
+  <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+  <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css" />  
 
   <?php if ($controller == 'customers' && ($action == 'create') || ($action == 'edit') || ($action == 'view')) : ?>
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/select2/css/select2.min.css">
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/dist/css/adminlte.min.css" />
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/bs-stepper/css/bs-stepper.min.css" />
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css" />
     <link rel="stylesheet" href="<?php echo ROOT; ?>assets/css/new_customer.css" />
   <?php endif; ?>
 
@@ -39,34 +24,24 @@
     <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css" />
     <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css" />
     <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css" />
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/select2/css/select2.min.css">
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/dist/css/adminlte.min.css" />
+  <?php endif; ?>
+
+  <?php if ($controller == 'users' && ($action == 'login')) : ?>
+    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/css/parallax.css" />
   <?php endif; ?>
 
   <?php if ($controller == 'orders' && ($action == 'create')) : ?>
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/select2/css/select2.min.css">
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/dist/css/adminlte.min.css" />
     <link rel="stylesheet" href="<?php echo ROOT; ?>assets/css/custom.css" />
   <?php endif; ?>
 
   <?php if ($controller == 'orders' && ($action == 'index')) : ?>
     <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css" />
     <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css" />
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css" />
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/select2/css/select2.min.css">
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/dist/css/adminlte.min.css" />
+    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css" /> 
     <link rel="stylesheet" href="<?php echo ROOT; ?>assets/css/custom.css" />
   <?php endif; ?>
 
   <?php if ($controller == 'invoices' && ($action == 'create')) : ?>
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/select2/css/select2.min.css">
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/select2/css/select2.min.css">
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/dist/css/adminlte.min.css" />
     <link rel="stylesheet" href="<?php echo ROOT; ?>assets/css/custom.css" />
   <?php endif; ?>
 
@@ -74,9 +49,6 @@
     <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css" />
     <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css" />
     <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css" />
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/select2/css/select2.min.css">
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/dist/css/adminlte.min.css" />
     <link rel="stylesheet" href="<?php echo ROOT; ?>assets/css/custom.css" />
   <?php endif; ?>
 
@@ -84,17 +56,10 @@
     <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css" />
     <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css" />
     <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css" />
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/select2/css/select2.min.css">
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/dist/css/adminlte.min.css" />
   <?php endif; ?>
 
-  <?php if ($controller == 'company' && ($action == 'create') || ($action == 'edit') || ($action == 'view')) : ?>
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/select2/css/select2.min.css">
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/dist/css/adminlte.min.css" />
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/bs-stepper/css/bs-stepper.min.css" />
-    <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css" />
-  <?php endif; ?>
-
+  <link rel="stylesheet" href="<?php echo ROOT; ?>assets/plugins/fontawesome-free/css/all.min.css" />
+  <link rel="stylesheet" href="<?php echo ROOT; ?>assets/dist/css/adminlte.min.css" />
+  <script src="<?php echo ROOT; ?>assets/plugins/jquery/jquery.min.js"></script>
+  <script src="<?php echo ROOT; ?>assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 </head>
