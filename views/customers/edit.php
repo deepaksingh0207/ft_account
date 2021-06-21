516e72fedf481a19a07f485e3bcbc56dbf1322f3
thajones <jones.thayil@outlook.com>
2021-06-21 21:32:48 +0530

    Update new_invoice.js
    1) Prepend a blank option with selection
    2)Deformatted Code causing tab error in address
    3) Removed Pan Keyup JS Code
    4) Rewritten Pan validation pattern

--- views/customers/edit.php
+++ views/customers/edit.php
@@ -65,9 +65,7 @@
                         <label for="id_address"> Address </label>
                       </div>
                       <div class="col-sm-12 col-lg-3 form-group">
-                        <textarea class="form-control fc ftsm" name="address" id="id_address" cols="30" rows="3">
-                          <?php echo trim($customer['address']) ?>
-                        </textarea>
+                        <textarea class="form-control fc ftsm" name="address" id="id_address" cols="30" rows="3"><?php echo trim($customer['address']) ?></textarea>
                       </div>
                     </div>
                     <div class="row mx-1">
@@ -101,8 +99,8 @@
                         <label for="id_pan"> PAN No. </label>
                       </div>
                       <div class="col-sm-12 col-lg-3 form-group">
-                        <input type="text" class="form-control fc ftsm pan" name="pan" id="id_pan"
-                          pattern="[A-Z]{5}[0-9]{4}[A-Z]{1}" minlength="10" maxlength="10"
+                        <input type="text" class="form-control fc ftsm" name="pan" id="id_pan"
+                          pattern="^[A-Z]{5}[0-9]{4}[A-Z]{1}+$" minlength="10" maxlength="10"
                           value="<?php echo $customer['pan'] ?>" />
                       </div>
                     </div>
