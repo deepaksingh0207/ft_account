2dad4f4778f0808aba92334284c73ffcb11c9588
thajones <jones.thayil@outlook.com>
2021-06-28 23:07:49 +0530

    design

--- views/payments/create.php
+++ views/payments/create.php
@@ -24,69 +24,9 @@
                   </div>
                   <div class="card-body p-3">
                     <div class="row mx-1">
+
                       <div class="col-12">
-                        <div class="card">
-                          <div class="card-header">
-                            <div class="row">
-                              <div class="col-sm-12 col-lg-2 pt-1">
-                                <label for="id_invoice_no">
-                                  Invoice Number :
-                                </label>
-                              </div>
-                              <div class="col-sm-12 col-lg-3 form-group mb-0">
-                                <select class="form-control select2" name="invoice_id" id="id_invoice_no">
-                                  <option value="">&nbsp;</option>'
-                                  <?php foreach ($invoices as $invoice) : ?>
-                                  <option value="<?php echo $invoice['id'] ?>">
-                                    <?php echo $invoice['id'] ?>
-                                  </option>
-                                  <?php endforeach; ?>
-                                </select>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="card-body" id="table_one" style="display: none;">
-                            <table class="table mb-0">
-                              <thead>
-                                <th>Basic Value</th>
-                                <th>GST Amount</th>
-                                <th>Total Invoice Amount</th>
-                                <th>TDS %</th>
-                                <th>Less TDS</th>
-                              </thead>
-                              <tbody>
-                                <input type="hidden" class="form-control" name="basic_value" id="id_basic_value"
-                                  value="80000">
-                                <td id="id_basicvalue" class="max150">
-                                  80000
-                                </td>
-                                <input type="hidden" class="form-control" name="gst_amount" id="id_gst_amount">
-                                <td id="id_gstamount" class="max150">
-                                  4400
-                                </td>
-                                <input type="hidden" class="form-control" name="invoice_amount" id="id_invoice_amount">
-                                <td id="id_invoiceamount" class="max150">
-                                  94400
-                                </td>
-                                <td class="max150 py-1">
-                                  <input type="number" class="form-control" name="tds_percent" min="0"
-                                    id="id_tds_percent" style="width: 75px;">
-                                </td>
-                                <input type="hidden" class="form-control" name="tds_deducted" id="id_tds_deducted">
-                                <td id="id_tdsdeducted" class="max150">
-                                </td>
-                              </tbody>
-                            </table>
-                          </div>
-                          <div class="card-footer" id="receivable_amt_div" style="display: none;">
-                            <div class="text-right">
-                              <input type="hidden" class="form-control" name="receivable_amt" id="id_receivable_amt">
-                              <b>Net Receivable Amount : </b>
-                              <span id="id_receivableamt">₹2,55,000.00</span>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="card" id="table_two" style="display: none;">
+                        <div class="card" id="table_two" style="display: block;">
                           <div class="card-body">
                             <table class="table mb-0">
                               <thead>
@@ -103,11 +43,12 @@
                                   <input type="text" class="form-control www" name="cheque_utr_no" id="id_cheque">
                                 </td>
                                 <td class="form-group">
-                                  <input type="number" class="form-control www" name="received_amt" min="1" id="id_received_amt">
+                                  <input type="number" class="form-control www" name="received_amt" min="1"
+                                    id="id_received_amt">
                                 </td>
                                 <!-- <td class="form-group">
-                                  <input type="number" class="form-control" name="allocated_amt" min="1" id="id_allocated_amt">
-                                </td> -->
+                                <input type="number" class="form-control" name="allocated_amt" min="1" id="id_allocated_amt">
+                              </td> -->
                               </tbody>
                             </table>
                           </div>
@@ -120,6 +61,82 @@
                           </div>
                         </div>
                       </div>
+
+                      <div class="col-12">
+                        <div class="card">
+                          <div class="card-header">
+                            <div class="row">
+                              <div class="col-sm-12 col-lg-2 pt-1">
+                                <label for="id_invoice_no">
+                                  Invoice Number :
+                                </label>
+                              </div>
+                              <div class="col-sm-12 col-lg-3 form-group mb-0">
+                                <select class="form-control select2" name="invoice_id[]" id="id_invoice_no1"
+                                  data-row="1">
+                                  <option value="">&nbsp;</option>'
+                                  <?php foreach ($invoices as $invoice) : ?>
+                                  <option value="<?php echo $invoice['id'] ?>">
+                                    <?php echo $invoice['id'] ?>
+                                  </option>
+                                  <?php endforeach; ?>
+                                </select>
+                              </div>
+                              <div class="col-sm-12 col-lg-7 text-right pt-1">
+                                <button type="button" class="btn btn-sm btn-primary" data-row="1">Add New</button>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="card-body" id="table_one1" style="display: block;">
+                            <table class="table mb-0">
+                              <thead>
+                                <th>Basic Value</th>
+                                <th>GST Amount</th>
+                                <th>Total Invoice Amount</th>
+                                <th>TDS %</th>
+                                <th>Less TDS</th>
+                                <th>Net Receivable Amount </th>
+                              </thead>
+                              <tbody>
+                                <tr>
+                                  <input type="hidden" data-row="1" name="basic_value[]" id="id_basic_value1">
+                                  <td id="id_basicvalue1" class="max150"></td>
+                                  <input type="hidden" data-row="1" name="gst_amount[]" id="id_gst_amount1">
+                                  <td id="id_gstamount1" class="max150"></td>
+                                  <input type="hidden" data-row="1" name="invoice_amount[]" id="id_invoice_amount1">
+                                  <td id="id_invoiceamount1" class="max150"></td>
+                                  <td class="max150 py-1">
+                                    <input type="number" data-row="1" class="form-control" name="tds_percent[]" min="0"
+                                      id="id_tds_percent1">
+                                  </td>
+                                  <input type="hidden" data-row="1" name="tds_deducted[]" id="id_tds_deducted1">
+                                  <td id="id_tdsdeducted1" class="max150"></td>
+                                  <input type="hidden" data-row="1" name="receivable_amt[]" id="id_receivable_amt1">
+                                  <td id="id_receivableamt1"></td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </div>
+                          <div class="card-footer" id="receivable_amt_div1" style="display: block;">
+                            <div class="row">
+                              <div class="col-2 pt-2 text-center">
+                                <b>Allocated Amount : </b>
+                              </div>
+                              <div class="col-3">
+                                <input type="number" data-row="1" class="form-control" name="allocated_amt[]"
+                                  id="id_allocated_amt1">
+                              </div>
+                              <div class="col-7">
+                                <div class="text-right pt-2">
+                                  <input type="hidden" data-row="1" name="balance_amt[]" id="id_balance_amt1">
+                                  <b>Balance Amount : </b><span id="id_balanceamt1"></span>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
                     </div>
                   </div>
                   <div class="card-footer text-right">
@@ -132,8 +149,8 @@
                   </div>
                 </div>
 
-                <button type="button" id="responsemodal" class="btn btn-default" data-toggle="modal"
-                  data-target="#modal-sm" style="display: none"></button>
+                <button type="button" id="responsemodal" data-toggle="modal" data-target="#modal-sm"
+                  style="display: none"></button>
 
                 <div class="modal fade" id="modal-sm">
                   <div class="modal-dialog modal-sm">
