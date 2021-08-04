420d2b38cdbb52bc902b27f5632c05b2eba23a25
thajones <jones.thayil@outlook.com>
2021-08-04 11:05:04 +0530

    Update new_invoice.js

--- assets/js/new_invoice.js
+++ assets/js/new_invoice.js
@@ -442,6 +442,18 @@ function checker() {
 }
 
 $(document).on("click", ".pdf", function () {
-  $("#modal_body").empty().append('<embed src="' + $(this).data('href') + '" type="application/pdf" style="width: 100%; height: 513px;">');
+  url = $(this).data('href');
+  error = '<div class="error-page"><h2 class="headline text-warning"> 404</h2> <div class="error-content pt-4"> <h3><i class="fas fa-exclamation-triangle text-warning"></i> Oops! Invoice not found.</h3><p>We could not find the invoice you were looking for.</p> </div></div>'
+  $.get(url)
+    .done(function (responseText) {
+      a = responseText
+      if (a.search("Customer List") < 0) {
+        $("#modal_body").empty().append('<embed src="' + url + '" type="application/pdf" style="width: 100%; height: 513px;">');
+      } else {
+        $("#modal_body").empty().append(error);
+      }
+    }).fail(function () {
+      $("#modal_body").empty().append(error);
+    });
   $("#modelpdf").click();
 });
