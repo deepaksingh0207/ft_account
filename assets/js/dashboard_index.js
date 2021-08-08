4f480a9c65b2bae6afab0c63533db624180c02d4
thajones <jones.thayil@outlook.com>
2021-08-09 01:55:43 +0530

    dashboard closed

--- assets/js/dashboard_index.js
+++ assets/js/dashboard_index.js
@@ -1,14 +1,6 @@
+var dtable;
+
 $(function () {
-    $("#example1")
-        .DataTable({
-            responsive: true,
-            lengthChange: false,
-            autoWidth: false,
-            paging: true,
-            ordering: false,
-            searching: false,
-        });
-    // $(".odd").css("background-color", "transparent");
     $.each(invoicelist, function (index, value) {
         duedate = $("#due" + value).text();
         diff = appendcode(datediff(getMonth(duedate.split(" ")[2]) + "/" + duedate.split(" ")[1] + "/" + duedate.split(" ")[3]));
@@ -22,21 +14,58 @@ $(function () {
             $("#due" + value).append('<span class="description-percentage text-' + diff[0] + '">' + duedate + '</span>');
         }
     });
-    // $('.odd').hover(function () {
-    //     $(this).css('background-color', 'lightblue');
-    // }, function () {
-    //     $(this).css('background-color', 'transparent');
-    // });
-    // $('.even').hover(function () {
-    //     $(this).css('background-color', 'lightblue');
-    // }, function () {
-    //     $(this).css('background-color', 'transparent');
-    // });
+    $(".select2").select2();
+    // fill_datatable();
 });
 
-$(".sublist").click(function () {
-    var parent_id = $(this).parent("tr").attr("data-href");
-    window.location = parent_id;
+function fill_datatable(appliedfilter = {}) {
+    dtable = $("#example1").DataTable({
+        "processing": true,
+        "ordering": false,
+        "bLengthChange": false,
+        "pageLength": 10,
+        "order": [],
+        "searching": false,
+        "columns": [ //data: 0 => invoice_id
+            { data: 1 },
+            { data: 2 },
+            { data: 3 },
+            { data: 4 },
+            { data: 5 }
+        ],
+        createdRow: function (row, data, dataIndex) {
+            $(row).attr('data-href', data[0]).children('td').addClass('sublist pointer align-middle text-center');
+        },
+        "ajax": {
+            url: baseUrl + "dashboard/search/",
+            type: "POST",
+            data: appliedfilter
+        }
+    });
+}
+
+$("#id_startdate").on("change", function () {
+    $("#id_enddate").attr('min', $(this).val());
+});
+
+$(document).on("click", ".sublist", function () {
+    var parent_id = $(this).parent("tr").data('href');
+    window.location = baseUrl + 'invoices/view/' + parent_id;
+});
+
+$(".update").on("click", function () {
+    var f = {};
+    if ($("#id_startdate").val()) {
+        f.startdate = $("#id_startdate").val()
+    }
+    if ($("#id_enddate").val()) {
+        f.enddate = $("#id_enddate").val()
+    }
+    if ($("#id_customer").val()) {
+        f.customer_id = $("#id_customer").val()
+    }
+    dtable.destroy();
+    fill_datatable(f);
 });
 
 function appendcode(val) {
--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -3,102 +3,83 @@
     <?php include HOME . DS . 'includes' . DS . 'menu.inc.php'; ?>
     <div class="content-wrapper">
       <section class="content">
-        <div class="container-fluid pb-5">
-          <div class="row mt-3">
-            <div class="col-12">
-              <div class="card">
-                <div class="card-body">
-                  <div class="row">
-                    <!-- <div class="col-sm-12 col-lg-2 form-group">
-                      <label for="id_period"> Period : </label>
-                      <select class="form-control fc ftsm mt-0" name="period" id="id_period">
-                        <option value="1">All</option>
-                        <option value="2">Custom Period</option>
-                        <option value="3">Today</option>
-                        <option value="4">Yesterday</option>
-                        <option value="5">Today</option>
-                        <option value="6">This Week</option>
-                        <option value="7">Last Week</option>
-                        <option value="8">This Month</option>
-                        <option value="9">Last Month</option>
-                      </select>
-                    </div> -->
-                    <div class="col-sm-12 col-lg-5">
-                      <div class="row">
-                        <div class="col-sm-12 col-lg-6">
-                          <label for="id_startdate"> Start Date :</label>
-                          <input type="date" class="form-control ftsm" name="startdate" id="id_startdate"
-                            disabled="True" />
-                        </div>
-                        <div class="col-sm-12 col-lg-6">
-                          <label for="id_enddate"> End Date :</label>
-                          <input type="date" class="form-control ftsm" name="enddate" id="id_enddate" disabled="True" />
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-sm-12 col-lg-2 form-group">
-                      <label for="id_customer"> Customer : </label>
-                      <select class="form-control fc ftsm select2 mt-0" name="customer" id="id_customer">
-                        <option value=""></option>
-                        <option value="">Customer A</option>
-                      </select>
-                    </div>
-                    <div class="col-sm-12 col-lg-3 pt-2">
-                      <br />
-                      <button class="btn btn-primary update" type="button">
-                        Update
-                      </button>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-sm-12 col-lg-12">
-                      <table id="example1" class="table table-striped table-hover table-bordered">
-                          <thead>
-                            <tr>
-                              <th rowspan="2">Customer</th>
-                              <th colspan="3" class="text-center" style="border-bottom: 0px;">Invoice</th>
-                              <th rowspan="2">Received Amount</th>
-                              <th rowspan="2">Balance Amount</th>
-                              <th rowspan="2">Due Date</th>
-                              <th rowspan="2" rowspan="2">Ageing</th>
-                            </tr>
-                            <tr>
-                              <th>No.</th>
-                              <th class="text-center">Date</th>
-                              <th>Amount</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <?php if (is_array($invoices) || is_object($invoices)) : ?>
-                            <?php foreach ($invoices as $invoice) : ?>
-                            <tr data-href="<?php echo ROOT; ?>invoices/view/<?php echo $invoice['invoice_id'] ?>">
-                              <td class="align-middle text-center">
-                                <?php echo $invoice['customer_name'] ?>
-                              </td>
-                              <td class="sublist pointer align-middle text-center">
-                                <?php echo $invoice['invoice_id'] ?>
-                              </td>
-                              <td class="sublist pointer align-middle text-center">
-                                <?php echo date('d, M Y', strtotime($invoice['invoice_date'])) ?>
-                              </td>
-                              <td class="sublist pointer align-middle text-center">
-                                <?php echo $invoice['invoice_amount'] ?>
-                              </td>
-                              <td class="sublist pointer align-middle text-center">
-                                <?php echo $invoice['recieved_amount'] ?>
-                              </td>
-                              <td class="sublist pointer align-middle text-center">
-                                <?php echo $invoice['balance_amount'] ?>
-                              </td>
-                              <td class="sublist pointer align-middle text-center" id="due<?php echo $invoice['invoice_id'] ?>"><?php echo date('D, d M Y', strtotime($invoice['due_date'])) ?></td>
-                              <td id="age<?php echo $invoice['invoice_id'] ?>"></td>
-                            </tr>
-                            <?php endforeach; ?>
-                            <?php endif; ?>
-                          </tbody>
-                      </table>
-                    </div>
-                  </div>
+        <div class="container-fluid mt-2">
+          <div class="card">
+            <div class="card-body">
+              <div class="row">
+                <div class="col-sm-12 col-lg-3">
+                  <label for="id_startdate"> Start Date :</label>
+                  <input type="date" class="form-control ftsm" name="startdate" id="id_startdate">
+                </div>
+                <div class="col-sm-12 col-lg-3">
+                  <label for="id_enddate"> End Date :</label>
+                  <input type="date" class="form-control ftsm" name="enddate" id="id_enddate">
+                </div>
+                <div class="col-sm-12 col-lg-3">
+                  <label for="id_customer"> Customer : </label>
+                  <select class="form-control fc ftsm select2 mt-0" name="customer" id="id_customer">
+                    <option value="">Select Customer</option>
+                    <?php foreach ($customers as $customer) : ?>
+                    <option value="<?php echo $customer['id'] ?>">
+                      <?php echo $customer['name'] ?>
+                    </option>
+                    <?php endforeach; ?>
+                  </select>
+                </div>
+                <div class="col-sm-12 col-lg-3 mt-4">
+                  <button class="btn btn-primary update mt-2" type="button">
+                    Search
+                  </button>
+                </div>
+                <div class="col-sm-12 col-lg-12 mt-3">
+                  <table id="example1" class="table table-striped table-hover table-bordered">
+                    <thead>
+                      <tr>
+                        <th rowspan="2">Customer</th>
+                        <th colspan="3" class="text-center" style="border-bottom: 0px;">Invoice</th>
+                        <th rowspan="2">Received Amount</th>
+                        <th rowspan="2">Balance Amount</th>
+                        <th rowspan="2">Due Date</th>
+                        <th rowspan="2" rowspan="2">Ageing</th>
+                      </tr>
+                      <tr>
+                        <th>No.</th>
+                        <th class="text-center">Date</th>
+                        <th>Amount</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <?php if (is_array($invoices) || is_object($invoices)) : ?>
+                      <?php foreach ($invoices as $invoice) : ?>
+                      <tr data-href="<?php echo ROOT; ?>invoices/view/<?php echo $invoice['invoice_id'] ?>">
+                        <td class="align-middle text-center">
+                          <?php echo $invoice['customer_name'] ?>
+                        </td>
+                        <td class="sublist pointer align-middle text-center">
+                          <?php echo $invoice['invoice_id'] ?>
+                        </td>
+                        <td class="sublist pointer align-middle text-center">
+                          <?php echo date('d, M Y', strtotime($invoice['invoice_date'])) ?>
+                        </td>
+                        <td class="sublist pointer align-middle text-center">
+                          <?php echo $invoice['invoice_amount'] ?>
+                        </td>
+                        <td class="sublist pointer align-middle text-center">
+                          <?php echo $invoice['recieved_amount'] ?>
+                        </td>
+                        <td class="sublist pointer align-middle text-center">
+                          <?php echo $invoice['balance_amount'] ?>
+                        </td>
+                        <td class="sublist pointer align-middle text-center"
+                          id="due<?php echo $invoice['invoice_id'] ?>">
+                          <?php echo date('D, d M Y', strtotime($invoice['due_date'])) ?>
+                        </td>
+                        <td id="age<?php echo $invoice['invoice_id'] ?>"></td>
+                      </tr>
+                      <?php endforeach; ?>
+                      <?php endif; ?>
+                    </tbody>
+                  </table>
                 </div>
               </div>
             </div>
@@ -110,7 +91,7 @@
     <script>
       var invoicelist = []
       <?php if (is_array($invoices) || is_object($invoices)) : ?>
-      <?php foreach ($invoices as $invoice) : ?>
+      <?php foreach($invoices as $invoice) : ?>
       invoicelist.push(<?php echo $invoice['invoice_id'] ?>);
       <?php endforeach; ?>
       <?php endif; ?>
