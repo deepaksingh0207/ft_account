3d8e1a693012e501dd16a60b888dca515656fea8
thajones <jones.thayil@outlook.com>
2021-08-08 15:57:35 +0530

    number js

--- assets/js/new_invoice_validation.js
+++ assets/js/new_invoice_validation.js
@@ -102,3 +102,12 @@ $(function () {
     },
   });
 });
+
+$(".numberonly").on("keypress", function (event) {
+  var regex = new RegExp("^[0-9]$");
+  var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+  if (!regex.test(key)) {
+    event.preventDefault();
+    return false;
+  }
+});
