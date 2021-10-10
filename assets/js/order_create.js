0120dacca85a992daaab86d1dfdd7ec44b4523a2
Jones Thayil <jones.thayil@gmail.com>
2021-10-11 02:54:43 +0530

    Update order_create.js

--- assets/js/order_create.js
+++ assets/js/order_create.js
@@ -16,6 +16,7 @@ var editmode = true;
 var instance_list = [];
 var tree = { otl: [] };
 var fill_last_one = false;
+
 $("#group_id").change(function () {
   group_id_reset();
   group_id = $(this).val();
@@ -38,9 +39,11 @@ $("#group_id").change(function () {
       });
   }
 });
+
 function billto_reset() {
   $("#sales_person").val("");
 }
+
 function group_id_reset() {
   group_id = "";
   group_id_data = "";
@@ -51,6 +54,7 @@ function group_id_reset() {
   billto_reset();
   $("#customer_name").text("");
 }
+
 function fill_billto_details(index, id, name) {
   checked_billto = index;
   $("#bill_to").val(id).removeClass("is-invalid");
@@ -59,11 +63,13 @@ function fill_billto_details(index, id, name) {
   $("#customer_name").text(name);
   getcustomerdetails(id);
 }
+
 function fill_shipto_details(index, id) {
   checked_shipto = index;
   $("#ship_to").val(id).removeClass("is-invalid");
   $("#ship_to-error").remove();
 }
+
 function getcustomerdetails(id) {
   billto_reset();
   if (id) {
@@ -83,6 +89,7 @@ function getcustomerdetails(id) {
       });
   }
 }
+
 function getgst(id) {
   $.ajax({
     type: "POST",
@@ -115,22 +122,28 @@ function getgst(id) {
     $("#order_items_card").show();
   });
 }
+
 $("#billto_search_button").on("click", function () {
   $("#id_search_billto").trigger("click");
 });
+
 $("#billto_search_button").on("click", function () {
   modelfill("billto", "Bill To Address");
   $("#checkbox" + checked_billto).prop("checked", true);
   $("#row_" + checked_billto).css("background-color", "powderblue");
 });
+
 $("#ship_to").on("click", function () {
   $("#shipto_search_button").trigger("click");
 });
+
 $("#shipto_search_button").on("click", function () {
   modelfill("shipto", "Ship To Address");
   $("#checkbox" + checked_shipto).prop("checked", true);
   $("#row_" + checked_shipto).css("background-color", "powderblue");
+
 });
+
 function modelfill(checkboxclass, label) {
   $("#modal_title").text(label);
   $("#addhead").empty();
@@ -217,6 +230,7 @@ function modelfill(checkboxclass, label) {
     $("#addhead").append("No Records");
   }
 }
+
 $(document).on("click", ".fill_customer_details", function () {
   highlightrow($(this).data("index"));
   if ($(this).data("modal") == "billto") {
@@ -229,6 +243,7 @@ $(document).on("click", ".fill_customer_details", function () {
     fill_shipto_details($(this).data("index"), $(this).data("id"));
   }
 });
+
 function highlightrow(id) {
   $("#row_" + old_row).css("background-color", "inherit");
   $("#row_" + checked_billto).css("background-color", "inherit");
@@ -237,9 +252,11 @@ function highlightrow(id) {
   $("#checkbox" + id).prop("checked", true);
   $("#row_" + id).css("background-color", "powderblue");
 }
+
 $("#addy").change(function () {
   $(this).attr("disabled", "");
 });
+
 $("#po_no").change(function () {
   mydata = { customer_id: $("#customer_id").val(), po_no: $(this).val() };
   if ($(this).val()) {
@@ -269,10 +286,12 @@ $("#po_no").change(function () {
       });
   }
 });
+
 function paymentterm_reset() {
   paymentterm_list = [];
   $("#payment_term_cardbody").empty();
 }
+
 function ordertype_reset() {
   paymentterm_reset();
   instance_list = [];
@@ -292,6 +311,7 @@ function ordertype_reset() {
   paymentterm_list = [];
   $("#add_item").trigger("click");
 }
+
 function nz(val) {
   // NaN to Zero
   if (val == "" || val == NaN) {
@@ -300,6 +320,7 @@ function nz(val) {
     return parseInt(val);
   }
 }
+
 $(document).on("change", "#order_type", function () {
   $(this).removeClass("is-invalid");
   ordertype_reset();
@@ -350,7 +371,9 @@ $(document).on("change", "#order_type", function () {
     }
     $("#add_order_cardbody").show();
   }
+
 });
+
 $(document).on("click", "#add_order_item_button", function () {
   if (editmode) {
     new_oii = orderitem_list.length + 1;
@@ -364,6 +387,7 @@ $(document).on("click", "#add_order_item_button", function () {
     add_order(new_oii);
   }
 });
+
 function add_order(id) {
   if (oti < 5) {
     $("#order_item_list").append(
@@ -475,6 +499,7 @@ function add_order(id) {
     payment_term_cardbody(id);
   }
 }
+
 function otiuom(id = false, ot = oti) {
   oti_uomid_list = ["", "2", "3", "2", "1", "", ""];
   if (id) {
@@ -483,10 +508,12 @@ function otiuom(id = false, ot = oti) {
   oti_uom_list = ["", "AU", "Percentage (%)", "AU", "Day(s)", "", ""];
   return oti_uom_list[ot];
 }
+
 function getuom(val) {
   uom_list = ["", "Day(s)", "AU", "Percentage (%)", "PC"];
   return uom_list[val];
 }
+
 function getot(val) {
   ot_list = [
     "",
@@ -499,6 +526,7 @@ function getot(val) {
   ];
   return ot_list[val];
 }
+
 function payment_term_cardbody(id) {
   if (oti == 1 || oti == 3) {
     $("#payment_term_cardbody").append(
@@ -523,6 +551,7 @@ function payment_term_cardbody(id) {
   }
   add_paymentterm(id, 1);
 }
+
 function add_paymentterm(oid, pid) {
   if (oti == 2) {
     $("#paymentterm_list_" + oid).append(
@@ -630,6 +659,7 @@ function add_paymentterm(oid, pid) {
     );
   }
 }
+
 $(document).on("change", ".paymentterm_quantity", function () {
   var oid = $(this).data("oid");
   var pid = $(this).data("pid");
@@ -646,10 +676,12 @@ $(document).on("change", ".paymentterm_quantity", function () {
   }
   lastfill(oid);
 });
+
 $(document).on("click", "i.trash", function () {
   del_id = $(this).data("id");
   $("#modelactivate").click();
 });
+
 $(".killrow").click(function () {
   $("#order_item_" + del_id).remove();
   orderitem_list = jQuery.grep(orderitem_list, function (b) {
@@ -664,6 +696,7 @@ $(".killrow").click(function () {
   }
   $("#byemodal").click();
 });
+
 function gen_paymentterm(id, value) {
   if (oti < 4 && editmode) {
     paymentterm_list = tree[oti][id]["ptl"];
@@ -690,6 +723,7 @@ function gen_paymentterm(id, value) {
     }
   }
 }
+
 $(document).on("change", ".order_item_quantity", function () {
   if (oti < 4) {
     gen_paymentterm($(this).data("id"), $(this).val());
@@ -697,12 +731,15 @@ $(document).on("change", ".order_item_quantity", function () {
     $(".item").trigger("change");
   }
 });
+
 $(document).on("change", ".order_item_unitprice", function () {
   order_item_calculator($(this).data("id"));
 });
+
 $(document).on("change", ".order_item_uom", function () {
   order_item_calculator($(this).data("id"));
 });
+
 function order_item_calculator(id) {
   var a = $("#orderitem_" + id + "_val_3").val();
   var b = $("#orderitem_" + id + "_val_5").val();
@@ -747,6 +784,7 @@ function order_item_calculator(id) {
   $("#orderitem_" + id + "_txt_6").text(humanamount(c));
   order_calculator();
 }
+
 function order_calculator() {
   var sub_total = 0;
   $.each(instance_list, function (index, oid) {
@@ -766,10 +804,12 @@ function order_calculator() {
   $("#add_order_item_igst_txt").text(humanamount(c));
   $("#add_order_item_total_txt").text(humanamount(a + b + c + sub_total));
 }
+
 $(document).on("click", "#add_order_button", function () {
   $("#main_card").hide();
   $("#add_order_card").show();
 });
+
 $(document).on("click", ".showmain_card", function () {
   if ($(this).val() == 1) {
     checker();
@@ -781,13 +821,16 @@ $(document).on("click", ".showmain_card", function () {
     $("#order_type").val("");
   }
 });
+
 function showmain() {
   $("#main_card").show();
   $("#add_order_card").hide();
 }
+
 $(document).on("change", "#id_po_from_date", function () {
   $("#id_po_to_date").attr("min", $(this).val());
 });
+
 function treeleaves() {
   $.each(instance_list, function (index, oid) {
     tree[oti][oid]["itm"] = $("#orderitem_" + oid + "_val_1").val();
@@ -818,7 +861,11 @@ function treeleaves() {
         ).val();
       });
     } else {
-      tree[oti][oid]["uom"] = $("#orderitem_" + oid + "_val_4").val();
+      if (oti == 4) {
+        tree[oti][oid]["uom"] = "1";
+      } else {
+        tree[oti][oid]["uom"] = $("#orderitem_" + oid + "_val_4").val();
+      }
     }
   });
   treecleaner();
@@ -826,6 +873,7 @@ function treeleaves() {
   ordertype_reset();
   $("#order_type").val("");
 }
+
 function treecleaner() {
   if (tree["otl"].length > 0) {
     $.each(tree["otl"], function (index, val) {
@@ -873,6 +921,7 @@ function treecleaner() {
     });
   }
 }
+
 function treehouse() {
   $("#order_items").empty();
   $.each(tree["otl"], function (index, val) {
@@ -895,6 +944,7 @@ function treehouse() {
     });
   });
 }
+
 $(document).on("click", ".myorder", function () {
   $("#add_order_button").trigger("click");
   editmode = false;
@@ -946,6 +996,7 @@ $(document).on("click", ".myorder", function () {
   });
   editmode = true;
 });
+
 function form_maker() {
   var subtotal = 0
   $.each(tree["otl"], function (index, ot) {
@@ -981,12 +1032,12 @@ function form_maker() {
   b = (cgst / 100) * subtotal;
   c = (igst / 100) * subtotal;
   var total = subtotal + a + b + c;
-  if (gst.state == "same"){
+  if (gst.state == "same") {
     $("#hiddendata")
-    .append('<input type="hidden" name="taxrate" value="' + gst.sgst + '">')
+      .append('<input type="hidden" name="taxrate" value="' + gst.sgst + '">')
   } else {
     $("#hiddendata")
-    .append('<input type="hidden" name="taxrate" value="' + gst.igst + '">')
+      .append('<input type="hidden" name="taxrate" value="' + gst.igst + '">')
   }
   $("#hiddendata")
     .append('<input type="hidden" name="ordersubtotal" value="' + subtotal + '">')
@@ -995,6 +1046,7 @@ function form_maker() {
     .append('<input type="hidden" name="igst" value="' + c + '">')
     .append('<input type="hidden" name="ordertotal" value="' + total + '">');
 }
+
 function lastfill(oid) {
   var pt_total_qty = 0;
   var empty_qty_ids = [];
@@ -1029,9 +1081,11 @@ function lastfill(oid) {
     }
   }
 }
+
 $(document).on("change", "#from_date", function () {
   $("#to_date").attr("min", $(this).val());
 });
+
 $(document).on("change", ".item", function () {
   if (oti == 2) {
     $(".paymentterm_item").text($(this).val());
