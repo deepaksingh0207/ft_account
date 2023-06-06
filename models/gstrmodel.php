bb8ea9868cc36f34f6aa3d5216bab2a7091279d4
jthayil-ft <jonest@fts-pl.com>
2023-06-06 15:59:53 +0530

    Update gstrmodel.php

--- models/gstrmodel.php
+++ models/gstrmodel.php
@@ -3,7 +3,7 @@
 class GstrModel extends Model {
     
     public function getGstrReportList($id) {
-        $sql = "SELECT i.id, c.gstin, c.name receiver, i.invoice_no, LEFT(i.invoice_date , 10) invoice_date, i.sub_total invoice_value, CONCAT(LEFT(c.gstin , 2),'-',s.name) pos, 'N' rc, NULL tr, 'Regular B2B' it, NULL egst, 18 rate, invoice_total,NULL cs FROM invoices i inner join customers c on i.customer_id = c.id inner join states s on s.id = c.state";
+        $sql = "SELECT i.id, c.gstin, c.name receiver, i.invoice_no, LEFT(i.invoice_date , 10) invoice_date, sum(t.total) invoice_value, CONCAT(LEFT(c.gstin , 2),'-',s.name) pos, 'N' rc, NULL tr, 'Regular B2B' it, NULL egst, 18 rate, invoice_total,NULL cs FROM invoices i inner join invoice_items t on t.invoice_id = i.id inner join customers c on i.customer_id = c.id inner join states s on s.id = c.state";
         if ($id) { $sql = $sql." where i.gstr_set = ".$id.";"; }
         else { $sql .= " where i.gstr_set is null "; }        
         $this->_setSql($sql);
